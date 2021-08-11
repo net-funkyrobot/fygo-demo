19b90acf526e7c6b220f0fa3abf243664d9e91f5
Christopher Smith <chris@funkyrobot.net>
2021-08-11 18:31:14 +0100

    Configure DRF

--- demo/demo/settings/base.py
+++ demo/demo/settings/base.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -94,3 +95,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+
+REST_FRAMEWORK = {
+    # Ensure all access is authenticated
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"]
+}
